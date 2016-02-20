584bfb80f04bdeb69b01c0c8aa77b40139f4522d
Sotiris Ioannou <ijsotiris@gmail.com>
2016-02-20 10:35:30 -0500

    Added title description

--- js/script.js
+++ js/script.js
@@ -3,12 +3,25 @@ $( document ).ready(function() {
   /* Ajax call to get the data from the webserver */
   $.getJSON( "https://proto.io/en/jobs/candidate-questions/quiz.json",
     function( data ) {
-  	   //callback function to ensure that ajax has finished doing it's work
+      //callback function to ensure that ajax has finished doing it's work
+      init(data);
       showQuestion(data);
-
   });
 
-  
+  function init(data){
+    console.log(data);
+    $("#title").append(data.title);
+    $("#description").append(data.description);
+
+  }
+
+  //show a question
+  function showQuestion(data){
+
+
+
+
+  }
 
 
 });
